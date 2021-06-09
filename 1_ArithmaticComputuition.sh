729b5276cdeb77d326a74d1c7ea39fde8bdd9ed7
name <varunbrahmane30@gmail.com>
2021-06-09 16:43:42 +0530

    User input a,b,c

---
+++ 1_ArithmaticComputuition.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+
+read -p "Enter a value :: "  a 
+
+read -p "Enter b value :: "  b
+
+read -p "Enter c value :: "  c 
+
+
+echo " a value is : " $a
+echo " b value is : " $b
+echo " c value is : " $c
