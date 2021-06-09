de01c159e003a5fb0c3f168c162b15577ad3f854
name <varunbrahmane30@gmail.com>
2021-06-09 17:06:53 +0530

    Compute (c+a/b)

--- 1_ArithmaticComputuition.sh
+++ 1_ArithmaticComputuition.sh
@@ -12,7 +12,9 @@ echo " a value is : " $a
 echo " b value is : " $b
 echo " c value is : " $c
 
- result1=$(( $a+$b*$c ))
+ result1=$(( a+b*c ))
  result2=$(( a*b+c ))
+ result3=$(( c+a/b ))
 echo $result1;
 echo $result2;
+echo $result3;
